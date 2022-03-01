9e8d65688ef681bdfd1035298e71862fcefcd2a7
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-03-02 01:36:23 +0600

    Error solved

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
-(function ($) {
+;(function ($) {
 	"use strict";
 
-    jQuery(document).ready(function($){
-        jQuery('.lea-hero-slider').slick({
-            dots: true,
-            arrows: false,
-            autoplay: true,
-            autoplaySpeed: 8000
-        });
-    });
     
-    jQuery(window).load(function(){
-        
-    });
 }(jQuery));	
