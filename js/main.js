9c538ba988a1fca1efe293851d634f3f84b97fa7
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-03-01 22:10:14 +0600

    Project files including

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        jQuery('.lea-hero-slider').slick({
+            dots: true,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 8000
+        });
+    });
+    
+    jQuery(window).load(function(){
+        
+    });
+}(jQuery));	
